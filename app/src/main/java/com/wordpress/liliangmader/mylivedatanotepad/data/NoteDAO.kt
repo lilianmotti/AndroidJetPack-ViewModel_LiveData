ec9b3a5a23508ffb876c7f7368398e0073827c6d
LiGMA <lilianmotti@users.noreply.github.com>
2019-02-04 16:51:30 +0000

    add DAO for Notes

--- app/src/main/java/com/wordpress/liliangmader/mylivedatanotepad/data/NoteDAO.kt
+++ app/src/main/java/com/wordpress/liliangmader/mylivedatanotepad/data/NoteDAO.kt
@@ -1,2 +1,28 @@
 package com.wordpress.liliangmader.mylivedatanotepad.data
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+
+//Data Access Object, update later with ROOM and SQLite database
+class NoteDAO {
+    //prepare a database table
+
+    //MutableLiveData from Architecture Components Library
+    private val noteList = mutableListOf<Note>()
+
+    //LiveData is observed for changes and updates the UI
+    private val notes = MutableLiveData<List<Note>>()
+
+    init{
+        //connect the noteList
+        notes.value = noteList
+    }
+
+    fun addNote(note: Note){
+        noteList.add(note)
+        notes.value = noteList
+    }
+
+    fun getNotes() = notes as LiveData<List<Note>>
+
+}
