fb9c183dd41d76aade57d71fcb9b60a5cabe35ed
LiGMA <lilianmotti@users.noreply.github.com>
2019-02-04 18:06:09 +0000

    start adding nav_graph and fragment layout

--- app/src/main/java/com/wordpress/liliangmader/mylivedatanotepad/ui/MainActivity.kt
+++ app/src/main/java/com/wordpress/liliangmader/mylivedatanotepad/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package com.wordpress.liliangmader.mylivedatanotepad.ui
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
 import com.wordpress.liliangmader.mylivedatanotepad.R
 
 class MainActivity : AppCompatActivity() {
@@ -10,4 +11,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onSupportNavigateUp() =
+        findNavController(R.id.nav_host_fragment).navigateUp()
+
 }
