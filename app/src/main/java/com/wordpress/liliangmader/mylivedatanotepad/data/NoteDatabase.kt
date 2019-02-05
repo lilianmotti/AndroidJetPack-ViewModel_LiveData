a727cba8a85e348eb6146bc771d8f89b2bb3e615
LiGMA <lilianmotti@users.noreply.github.com>
2019-02-05 12:17:03 +0000

    create class for note database

--- app/src/main/java/com/wordpress/liliangmader/mylivedatanotepad/data/NoteDatabase.kt
+++ app/src/main/java/com/wordpress/liliangmader/mylivedatanotepad/data/NoteDatabase.kt
@@ -1,2 +1,11 @@
 package com.wordpress.liliangmader.mylivedatanotepad.data
 
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = arrayOf(
+    Note::class),
+    version = 1)
+abstract class ChoiceDatabase : RoomDatabase() {
+    abstract fun noteDao(): NoteDAO
+}
