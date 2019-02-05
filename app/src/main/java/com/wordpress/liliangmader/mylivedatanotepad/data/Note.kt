9b0277cc9c76fb6772da879130955b157d3e6d54
LiGMA <lilianmotti@users.noreply.github.com>
2019-02-05 11:55:45 +0000

    add dependencies for room

--- app/src/main/java/com/wordpress/liliangmader/mylivedatanotepad/data/Note.kt
+++ app/src/main/java/com/wordpress/liliangmader/mylivedatanotepad/data/Note.kt
@@ -4,4 +4,5 @@ package com.wordpress.liliangmader.mylivedatanotepad.data
 //declare all the variables needed to compose a note, e.g. val noteText: String, val author: String, val date with a timestamp...
 class Note(val note: String)  {
     //additional functions and variables to be added later
+
 }
