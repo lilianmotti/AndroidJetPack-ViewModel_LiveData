abe347151d61587865e6d89905c369d05480502c
LiGMA <lilianmotti@users.noreply.github.com>
2019-02-05 15:45:48 +0000

    create NotesViewModel

--- app/src/main/java/com/wordpress/liliangmader/mylivedatanotepad/ui/NotesViewModel.kt
+++ app/src/main/java/com/wordpress/liliangmader/mylivedatanotepad/ui/NotesViewModel.kt
@@ -1,7 +1,12 @@
 package com.wordpress.liliangmader.mylivedatanotepad.ui
 
-import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModel
+import com.wordpress.liliangmader.mylivedatanotepad.data.Note
+import com.wordpress.liliangmader.mylivedatanotepad.data.NoteRepository
 
-class NotesViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class NotesViewModel( private val noteRepository: NoteRepository) : ViewModel() {
+
+    fun getNotes() = noteRepository.getNotes()
+
+    fun insertNote(note: Note) = noteRepository.insertNote(note)
 }
